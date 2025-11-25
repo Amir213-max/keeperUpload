340902db6687b6d73601ebada3c05f260f0e649c
Amir Ghareeb <amirghareeb593@gmail.com>
2025-11-25 11:45:28 +0200

    Update graphqlClient.js

--- app/lib/graphqlClient.js
+++ app/lib/graphqlClient.js
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
 const endpoint =
-  process.env.GRAPHQL_ENDPOINT || "https://api.keepersport.sa/graphql";
+  process.env.GRAPHQL_ENDPOINT || "https://keeper.in-brackets.online/graphql";
 
 export const graphqlClient = new GraphQLClient(endpoint, {
   headers: {
