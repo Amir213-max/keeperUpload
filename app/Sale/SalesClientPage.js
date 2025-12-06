49c21de9f4f038be349e66c0b92afc9c8730f33a
Amir Ghareeb <amirghareeb593@gmail.com>
2025-12-06 02:24:18 +0200

    Update SalesClientPage.js

--- app/Sale/SalesClientPage.js
+++ app/Sale/SalesClientPage.js
@@ -148,185 +148,185 @@ useEffect(() => {
 
   return (
     <div className={`bg-[#373e3e] ${isRTL ? "rtl" : "ltr"}`}>
-      <div className="grid pt-1 grid-cols-1 lg:grid-cols-5">
-        {/* Sidebar */}
-        <div className="hidden lg:block lg:col-span-1 bg-black h-auto">
-          <Sidebar
-            categories={categoriesWithProducts}
-            onSelectCategory={(catId) => {
-              if (catId === selectedCategoryId) {
-                setSelectedCategoryId(null);
-                setSelectedCategoryName(null);
-              } else {
-                setSelectedCategoryId(catId);
-              }
-            }}
-            isRTL={isRTL} // ✅ تمرير اتجاه اللغة
-          />
-        </div>
+      // <div className="grid pt-1 grid-cols-1 lg:grid-cols-5">
+//         {/* Sidebar */}
+//         <div className="hidden lg:block lg:col-span-1 bg-black h-auto">
+//           <Sidebar
+//             categories={categoriesWithProducts}
+//             onSelectCategory={(catId) => {
+//               if (catId === selectedCategoryId) {
+//                 setSelectedCategoryId(null);
+//                 setSelectedCategoryName(null);
+//               } else {
+//                 setSelectedCategoryId(catId);
+//               }
+//             }}
+//             isRTL={isRTL} // ✅ تمرير اتجاه اللغة
+//           />
+//         </div>
 
-        {/* Products Section */}
-        <div className="md:col-span-4 p-4 bg-white">
-          <h1 className="text-4xl text-[#1f2323] p-2">
-            {selectedCategoryName || t("Sale")}
-          </h1>
+//         {/* Products Section */}
+//         <div className="md:col-span-4 p-4 bg-white">
+//           <h1 className="text-4xl text-[#1f2323] p-2">
+//             {selectedCategoryName || t("Sale")}
+//           </h1>
 
-          <BrandsSlider
-            brands={brands}
-            selectedBrand={selectedBrand}
-            direction={isRTL ? "rtl" : "ltr"}
-            onBrandClick={(brand) =>
-              setSelectedBrand(brand === selectedBrand ? null : brand)
-            }
-          />
+//           <BrandsSlider
+//             brands={brands}
+//             selectedBrand={selectedBrand}
+//             direction={isRTL ? "rtl" : "ltr"}
+//             onBrandClick={(brand) =>
+//               setSelectedBrand(brand === selectedBrand ? null : brand)
+//             }
+//           />
 
-          <div className="flex mb-4 gap-3 flex-wrap">
-            <FilterDropdown
-              attributeValues={attributeValues}
-              onFilterChange={setSelectedAttributes}
-            />
-          </div>
+//           <div className="flex mb-4 gap-3 flex-wrap">
+//             <FilterDropdown
+//               attributeValues={attributeValues}
+//               onFilterChange={setSelectedAttributes}
+//             />
+//           </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-3 p-2 sm:p-4">
-  {currentProducts.map((product) => {
-    const basePrice = product.list_price_amount;
-    let finalPrice = product.price_range_exact_amount;
+//         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-3 p-2 sm:p-4">
+//   {currentProducts.map((product) => {
+//     const basePrice = product.list_price_amount;
+//     let finalPrice = product.price_range_exact_amount;
 
-    // لو فيه بادج فيها نسبة خصم زي "20%"
-    const badgeLabel = product.productBadges?.[0]?.label || "";
-    const discountMatch = badgeLabel.match(/(\d+)%/); // يجيب الرقم من "20%"
-    if (discountMatch) {
-      const discountPercent = parseFloat(discountMatch[1]);
-      finalPrice = basePrice - (basePrice * discountPercent) / 100;
-    }
+//     // لو فيه بادج فيها نسبة خصم زي "20%"
+//     const badgeLabel = product.productBadges?.[0]?.label || "";
+//     const discountMatch = badgeLabel.match(/(\d+)%/); // يجيب الرقم من "20%"
+//     if (discountMatch) {
+//       const discountPercent = parseFloat(discountMatch[1]);
+//       finalPrice = basePrice - (basePrice * discountPercent) / 100;
+//     }
 
-    return (
-      <Link
-        key={product.sku}
-        href={`/product/${encodeURIComponent(product.sku)}`}
-        className="relative bg-gradient-to-br from-white to-neutral-300 shadow-lg overflow-hidden flex flex-col transition-all duration-300 hover:shadow-xl hover:-translate-y-1 block group"
-        onClick={(e) => {
-          // اضغط على الصورة → افتح صفحة المنتج
-          if (e.target.closest(".product-image-click")) {
-            return; // يفتح عادي
-          }
+//     return (
+//       <Link
+//         key={product.sku}
+//         href={`/product/${encodeURIComponent(product.sku)}`}
+//         className="relative bg-gradient-to-br from-white to-neutral-300 shadow-lg overflow-hidden flex flex-col transition-all duration-300 hover:shadow-xl hover:-translate-y-1 block group"
+//         onClick={(e) => {
+//           // اضغط على الصورة → افتح صفحة المنتج
+//           if (e.target.closest(".product-image-click")) {
+//             return; // يفتح عادي
+//           }
 
-          // اضغط داخل السلايدر → امنع فتح اللينك
-          if (e.target.closest(".product-swiper-container")) {
-            e.preventDefault();
-            e.stopPropagation();  // ← مهم جدًا علشان ما يعليش bubbled click
-            return;
-          }
-        }}
-      >
-        {/* Badge */}
-        {badgeLabel && (
-          <div
-            className="absolute top-3 left-[-20px] w-[90px] text-center text-white text-xs font-bold py-1 rotate-[-45deg] shadow-md z-10"
-            style={{
-              backgroundColor: product.productBadges?.[0]?.color || "#888",
-            }}
-          >
-            {badgeLabel}
-          </div>
-        )}
+//           // اضغط داخل السلايدر → امنع فتح اللينك
+//           if (e.target.closest(".product-swiper-container")) {
+//             e.preventDefault();
+//             e.stopPropagation();  // ← مهم جدًا علشان ما يعليش bubbled click
+//             return;
+//           }
+//         }}
+//       >
+//         {/* Badge */}
+//         {badgeLabel && (
+//           <div
+//             className="absolute top-3 left-[-20px] w-[90px] text-center text-white text-xs font-bold py-1 rotate-[-45deg] shadow-md z-10"
+//             style={{
+//               backgroundColor: product.productBadges?.[0]?.color || "#888",
+//             }}
+//           >
+//             {badgeLabel}
+//           </div>
+//         )}
 
-        {/* صورة المنتج */}
-        <div className="flex justify-center items-center h-[220px]">
-          <ProductSlider images={product.images} productName={product.name} />
-        </div>
+//         {/* صورة المنتج */}
+//         <div className="flex justify-center items-center h-[220px]">
+//           <ProductSlider images={product.images} productName={product.name} />
+//         </div>
 
-        {/* تفاصيل المنتج */}
-        <div className="p-4 flex flex-col flex-grow justify-between">
-          <h3 className="text-base text-gray-700 text-center font-bold mb-1">
-            {product.brand?.name}
-          </h3>
+//         {/* تفاصيل المنتج */}
+//         <div className="p-4 flex flex-col flex-grow justify-between">
+//           <h3 className="text-base text-gray-700 text-center font-bold mb-1">
+//             {product.brand?.name}
+//           </h3>
 
-          <p className="text-center text-sm text-gray-500 line-clamp-2 mb-3">
-            {product.name}
-          </p>
+//           <p className="text-center text-sm text-gray-500 line-clamp-2 mb-3">
+//             {product.name}
+//           </p>
 
-          {/* السعر */}
-          <div className="text-center">
-            {discountMatch ? (
-              <>
-                <div className="line-through text-gray-500 text-sm">
-                  <PriceDisplay price={basePrice} loading={currencyLoading} />
-                </div>
-                <span className="text-lg font-bold text-neutral-900">
-                  <PriceDisplay price={finalPrice} loading={currencyLoading} />
-                </span>
-              </>
-            ) : (
-              <span className="text-lg font-bold text-neutral-900">
-                <PriceDisplay price={basePrice} loading={currencyLoading} />
-              </span>
-            )}
-          </div>
-        </div>
-      </Link>
-    );
-  })}
-</div>
+//           {/* السعر */}
+//           <div className="text-center">
+//             {discountMatch ? (
+//               <>
+//                 <div className="line-through text-gray-500 text-sm">
+//                   <PriceDisplay price={basePrice} loading={currencyLoading} />
+//                 </div>
+//                 <span className="text-lg font-bold text-neutral-900">
+//                   <PriceDisplay price={finalPrice} loading={currencyLoading} />
+//                 </span>
+//               </>
+//             ) : (
+//               <span className="text-lg font-bold text-neutral-900">
+//                 <PriceDisplay price={basePrice} loading={currencyLoading} />
+//               </span>
+//             )}
+//           </div>
+//         </div>
+//       </Link>
+//     );
+//   })}
+// </div>
 
-          {totalPages > 1 && (
-  <div className="flex justify-center items-center gap-4 mt-6 select-none">
-    {/* Previous Button */}
-    <button
-      onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
-      disabled={currentPage === 1}
-      className="px-3 py-2 cursour-pointer   bg-gray-200 text-gray-700 disabled:opacity-50 hover:bg-gray-300 transition"
-    >
-      &#10094;
-    </button>
+//           {totalPages > 1 && (
+//   <div className="flex justify-center items-center gap-4 mt-6 select-none">
+//     {/* Previous Button */}
+//     <button
+//       onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+//       disabled={currentPage === 1}
+//       className="px-3 py-2 cursour-pointer   bg-gray-200 text-gray-700 disabled:opacity-50 hover:bg-gray-300 transition"
+//     >
+//       &#10094;
+//     </button>
 
-    {/* Page numbers slider */}
-    <div className="flex gap-2  cursour-pointer overflow-x-auto scrollbar-none px-2">
-      {[...Array(totalPages)].map((_, idx) => {
-        const pageNumber = idx + 1;
-        // Show only few pages around current for slider effect
-        if (
-          pageNumber === 1 ||
-          pageNumber === totalPages ||
-          (pageNumber >= currentPage - 1 && pageNumber <= currentPage + 1)
-        ) {
-          return (
-            <button
-              key={idx}
-              onClick={() => setCurrentPage(pageNumber)}
-              className={`px-3 py-2   text-sm sm:text-base transition ${
-                currentPage === pageNumber
-                  ? "bg-[#1f2323] text-white shadow-md"
-                  : "bg-gray-100 text-gray-700 hover:bg-gray-200"
-              }`}
-            >
-              {pageNumber}
-            </button>
-          );
-        } else if (
-          pageNumber === currentPage - 2 ||
-          pageNumber === currentPage + 2
-        ) {
-          return <span key={idx} className="px-2 text-gray-500">...</span>;
-        } else {
-          return null;
-        }
-      })}
-    </div>
+//     {/* Page numbers slider */}
+//     <div className="flex gap-2  cursour-pointer overflow-x-auto scrollbar-none px-2">
+//       {[...Array(totalPages)].map((_, idx) => {
+//         const pageNumber = idx + 1;
+//         // Show only few pages around current for slider effect
+//         if (
+//           pageNumber === 1 ||
+//           pageNumber === totalPages ||
+//           (pageNumber >= currentPage - 1 && pageNumber <= currentPage + 1)
+//         ) {
+//           return (
+//             <button
+//               key={idx}
+//               onClick={() => setCurrentPage(pageNumber)}
+//               className={`px-3 py-2   text-sm sm:text-base transition ${
+//                 currentPage === pageNumber
+//                   ? "bg-[#1f2323] text-white shadow-md"
+//                   : "bg-gray-100 text-gray-700 hover:bg-gray-200"
+//               }`}
+//             >
+//               {pageNumber}
+//             </button>
+//           );
+//         } else if (
+//           pageNumber === currentPage - 2 ||
+//           pageNumber === currentPage + 2
+//         ) {
+//           return <span key={idx} className="px-2 text-gray-500">...</span>;
+//         } else {
+//           return null;
+//         }
+//       })}
+//     </div>
 
-    {/* Next Button */}
-    <button
-      onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
-      disabled={currentPage === totalPages}
-      className="px-3 py-2   bg-gray-200 text-gray-700 disabled:opacity-50 hover:bg-gray-300 transition"
-    >
-      &#10095;
-    </button>
-  </div>
-)}
+//     {/* Next Button */}
+//     <button
+//       onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
+//       disabled={currentPage === totalPages}
+//       className="px-3 py-2   bg-gray-200 text-gray-700 disabled:opacity-50 hover:bg-gray-300 transition"
+//     >
+//       &#10095;
+//     </button>
+//   </div>
+// )}
 
-        </div>
-      </div>
+//         </div>
+//       </div>
     </div>
   );
 }
